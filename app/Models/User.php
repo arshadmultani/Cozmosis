2ac3ebb33cf535bddb856941ce17a9f26a6c1ca9
arshadmultani <arshadrmultani@gmail.com>
2025-05-12 14:02:52 +0530

    add:Implements FilamentUser in User class

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+use Filament\Models\Contracts\FilamentUser; 
+use Filament\Panel;
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -38,11 +40,17 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
-    protected function casts(): array
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return str_ends_with($this->email, '@cozmosis.com') && $this->hasVerifiedEmail();
+    }
+     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
+
+    
 }
